ee319a4d74a2158891fae5b2d85986afdb91cec0
alainveylit <6517213+alainveylit@users.noreply.github.com>
2018-01-13 12:28:22 -0800

    Add files via upload

---
+++ jget.php
@@ -0,0 +1,10 @@
+<h4>Parameters passed to custom script jget.php:</h4>
+<p>Function: jget( {url: 'jget.php', params: { number: 2, menu-id: 'main-menu' } </p>
+	<p>Results from script: </p>
+	<code>
+<?php
+
+print_r($_GET);
+
+?>
+</code>
